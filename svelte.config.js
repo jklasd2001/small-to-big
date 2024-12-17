7bb047d3ba58c0c81d017a385713b7268b9b9852
jklasd2001 <jklasd2001@gmail.com>
2024-12-17 22:10:09 +0900

    feat: vercel adapter 추가

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import relativeImages from 'mdsvex-relative-images'
 import { mdsvex } from 'mdsvex'
 import { createHighlighter } from 'shiki'
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
