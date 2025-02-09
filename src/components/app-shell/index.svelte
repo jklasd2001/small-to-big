6e669b60b3ef566a8f7a82363be01120754651ab
jklasd2001 <jklasd2001@gmail.com>
2025-02-09 22:38:25 +0900

    feat: 라이브러리 업데이트

--- src/components/app-shell/index.svelte
+++ src/components/app-shell/index.svelte
@@ -4,23 +4,20 @@
   let { children } = $props()
 </script>
 
-<header class="fixed left-0 top-0 h-16 w-screen border-b border-b-base-300">
+<header class="border-b-base-300 fixed top-0 left-0 h-16 w-screen border-b">
   <div
     class="mx-auto flex h-full max-w-7xl items-center justify-between gap-4 px-4"
   >
-    <a href="/">
-      <img src="/logo.png" alt="logo" class="h-8 rounded-full" />
-    </a>
+    <a role="button" href="/" class="text-2xl font-bold">jklasd2001</a>
 
     <div class="flex items-center gap-4">
-      <a href="/birthday-letter" class="btn btn-ghost">편지</a>
-
-      <a href="/invitation-card" class="btn btn-ghost">초대장</a>
+      <a role="button" href="/invitation-card" class="btn">초대장</a>
 
       <a
         aria-label="Github"
-        class="btn btn-ghost"
+        class="btn btn-square"
         href="https://github.com/jklasd2001"
+        role="button"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -38,6 +35,30 @@
           ><path
             d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
           /><path d="M9 18c-4.51 2-5-2-7-2" />
+        </svg>
+      </a>
+
+      <a
+        aria-label="Email"
+        class="btn btn-square"
+        href="mailto:jklasd2001@gmail.com"
+        rel="noopener noreferrer"
+        role="button"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-mail"
+        >
+          <rect width="20" height="16" x="2" y="4" rx="2" />
+          <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
         </svg>
       </a>
     </div>
