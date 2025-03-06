71ff5a7cf8f93c2d7af770f98278f84457e32cf9
김영수 <youngsu.kim@gim-yeongsuui-MacBookPro.local>
2025-03-06 17:41:23 +0900

    feat: 즐겨찾기 사이트 정리

--- src/routes/(app)/favorites/+page.svelte
+++ src/routes/(app)/favorites/+page.svelte
@@ -1,4 +1,58 @@
 <script lang="ts">
 </script>
 
-<div>dkdkdk</div>
+<div>
+  <h1>Favorites</h1>
+
+  <div>Frontend</div>
+
+  <div>Radix UI</div>
+  <div>https://www.radix-ui.com/</div>
+
+  <div>Park UI</div>
+  <div>https://park-ui.com/</div>
+
+  <div>https://www.radix-ui.com/</div>
+
+  <div>Frontend Fundamentals</div>
+  <div>https://frontend-fundamentals.com/</div>
+
+  <div>usehooks.com</div>
+  <div>https://usehooks.com/</div>
+
+  <div>usehooks-ts</div>
+  <div>https://usehooks-ts.com/</div>
+
+  <div>Panda CSS</div>
+  <div>https://panda-css.com/</div>
+
+  <div>Ark UI</div>
+  <div>https://ark-ui.com/</div>
+
+  <div>Hero UI</div>
+  <div>https://www.heroui.com/</div>
+
+  <div>AriaKit</div>
+  <div>https://ariakit.org/</div>
+
+  <div>Next.js 한글 문서</div>
+  <div>https://nextjs-ko.org/docs</div>
+
+  <div>Tailwind Variants</div>
+  <div>https://www.tailwind-variants.org/</div>
+
+  <div>React Google Maps</div>
+  <div>https://visgl.github.io/react-google-maps/</div>
+
+  <div>블로그</div>
+
+  <div>https://emewjin.github.io/</div>
+  <div>https://maily.so/devpill</div>
+
+  <div>
+    https://velog.io/@jma1020/Next-NextJS-%ED%8E%98%EC%9D%B4%EC%A7%80%EA%B0%84-query-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%9D%B4%EB%8F%99%EB%B0%A9%EB%B2%95-Link-nextrouter
+  </div>
+
+  <div>GeekNews</div>
+  <div>https://news.hada.io/</div>
+</div>
